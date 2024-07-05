d88fb12ac0596ccd3bafa10e7a05ca2340e89aba
rajendherraju <38126617+rajendherraju@users.noreply.github.com>
2024-07-05 17:59:36 +0530

    Create ec2.tf
    ec2

---
+++ ec2.tf
@@ -0,0 +1,45 @@
+provider "aws" {
+  region = "ap-south-1"  # Change to your preferred region
+}
+
+resource "aws_key_pair" "my_key" {
+  key_name   = "my-key-pair"
+  public_key = file("my-key-pair.pem.pub")
+}
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+  
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "my_ec2" {
+  ami           = "ami-01376101673c89611"  # Replace with a valid AMI ID for your region
+  instance_type = "t2.micro"
+
+  key_name      = aws_key_pair.my_key.key_name
+
+  security_groups = [aws_security_group.allow_ssh.name]
+
+  tags = {
+    Name = "MyEC2Instance"
+  }
+}
+
+output "instance_public_ip" {
+  description = "The public IP of the web server"
+  value       = aws_instance.my_ec2.public_ip
+}
